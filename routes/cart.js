c9454782238f0684da110a4cab4b7e6af6c39e2e
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-04-23 22:07:55 +0300

    lesson 14

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,11 @@
 const {Router} = require('express')
 const router = Router()
-const Card = require('../models/cart')
+const Cart = require('../models/cart')
 const Course = require('../models/course')
 
 router.post('/add', async(req, res)=>{
     const course = await Course.getByID(req.body.id)
-    await Card.add(course)
+    await Cart.add(course)
     res.redirect('/cart')
 })
 
